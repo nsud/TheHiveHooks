c47db4f11c3b2400197aa1d5a7af8225e8f404b4
nsud <36475810+nsud@users.noreply.github.com>
2020-02-12 18:34:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from thehive_hooks import app
 
 def runserver():
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5055))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
